e4468d9b0c0d8f0a42eed9cbe0895952a9f25cfd
Wade Carlson <wcarlsondev@gmail.com>
2018-02-06 10:24:58 -0600

    added roles to users

---
+++ MKEWasteDisposal/Migrations/201802061613261_role.cs
@@ -0,0 +1,26 @@
+namespace MKEWasteDisposal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class role : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.RoleViewModels");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.RoleViewModels",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
