c360354973a55d2ed14933a04445b184bed67748
Wade Carlson <wcarlsondev@gmail.com>
2018-02-06 08:20:07 -0600

    adding file setup

---
+++ MKEWasteDisposal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MKEWasteDisposal.MvcApplication" Language="C#" %>
---
+++ MKEWasteDisposal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MKEWasteDisposal.Startup))]
+namespace MKEWasteDisposal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
