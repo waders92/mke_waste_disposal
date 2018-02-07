1d9716bacc39dc06feebcdb39dd2476af22d279e
Wade Carlson <wcarlsondev@gmail.com>
2018-02-07 12:00:18 -0600

    update way name is displayed

--- MKEWasteDisposal/Models/Customer.cs
+++ MKEWasteDisposal/Models/Customer.cs
@@ -11,6 +11,8 @@ namespace MKEWasteDisposal.Models
     {
         [Key]
         public int CustomerID { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
