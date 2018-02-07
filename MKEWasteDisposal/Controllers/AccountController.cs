28e680e246fddb7b67c9ec79986feefa5baa7374
Wade Carlson <wcarlsondev@gmail.com>
2018-02-07 13:31:06 -0600

    added authorize and a redirect

--- MKEWasteDisposal/Controllers/AccountController.cs
+++ MKEWasteDisposal/Controllers/AccountController.cs
@@ -95,7 +95,8 @@ namespace MKEWasteDisposal.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    //return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Home");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
--- MKEWasteDisposal/Controllers/CustomersController.cs
+++ MKEWasteDisposal/Controllers/CustomersController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity;
 
 namespace MKEWasteDisposal.Controllers
 {
+   [Authorize]
     public class CustomersController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
