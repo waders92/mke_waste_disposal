d2687ea3f2b826eece19069725075dad2834a53a
Wade Carlson <wcarlsondev@gmail.com>
2018-02-08 07:50:23 -0600

    added jquery date picker

--- MKEWasteDisposal/App_Start/BundleConfig.cs
+++ MKEWasteDisposal/App_Start/BundleConfig.cs
@@ -8,8 +8,6 @@ namespace MKEWasteDisposal
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,6 +20,9 @@ namespace MKEWasteDisposal
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                     "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
